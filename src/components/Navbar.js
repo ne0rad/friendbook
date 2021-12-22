2a72b7ce78f45472f163fcbd16a2361ab5479491
ne0rad <usackasr@gmail.com>
2021-12-22 15:58:09 +0000

    testing some material UI components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button'
 
 function Navbar() {
+    let navigate = useNavigate();
+    
     return (
         <div>
             <h1>
                 Welcome to FriendBook
             </h1>
+            <Button variant="contained" onClick={() => navigate("/about")}>Hello</Button>
+            <br />
             <Link to="/">Home</Link>
             <br />
             <Link to="/about">About</Link>
