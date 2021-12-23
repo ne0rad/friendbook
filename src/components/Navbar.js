d549a13411b3a1a942364a14644eba9501a00eb4
ne0rad <usackasr@gmail.com>
2021-12-23 18:17:34 +0000

    Updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,31 @@
-import { Link, useNavigate } from "react-router-dom";
-import Button from '@mui/material/Button'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
-function Navbar() {
+function NavBar() {
     let navigate = useNavigate();
-    
+
     return (
-        <div>
-            <h1>
-                Welcome to FriendBook
-            </h1>
-            <Button variant="contained" onClick={() => navigate("/about")}>Hello</Button>
-            <br />
-            <Link to="/">Home</Link>
-            <br />
-            <Link to="/about">About</Link>
-            <br />
-            <Link to="404">Not Found</Link>
-            <br />
-        </div>
-    )
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1, cursor: 'pointer' }}
+                        onClick={() => navigate("/")}
+                    >
+                        FriendBook
+                    </Typography>
+                    <Button color="inherit" onClick={() => navigate("/login")}>Login</Button>
+                    <Button color="inherit" onClick={() => navigate("/signup")}>Signup</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
 
-export default Navbar;
+export default NavBar;
