bb8a9a8f2b3f2c53f69cfe0379c12595f0e2cab0
ne0rad <usackasr@gmail.com>
2021-12-23 18:17:25 +0000

    Added login route

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import Container from "@mui/material/Container";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+      <Container maxWidth="lg" mt="1rem" align="center">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
