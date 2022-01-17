60bc707df6fcb800b00a25b50d6e43b856099cdc
ne0rad <usackasr@gmail.com>
2022-01-17 17:46:15 +0000

    Deleted About page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Container } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import About from "./pages/About";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
@@ -25,7 +24,6 @@ function App() {
       <Container maxWidth="lg" mt="1rem" align="center">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="*" element={<NotFound />} />
