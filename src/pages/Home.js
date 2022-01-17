6c51a809316851d7184dc29e5ee0abf7f51fc409
ne0rad <usackasr@gmail.com>
2022-01-17 13:30:03 +0000

    Updated Home, added links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,83 @@
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper } from '@mui/material';
+import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Typography } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 
 function Home() {
     return (
         <Box maxWidth="sm">
-            <Paper elevation={5} sx={{ mt: 3, p: 2 }}>
-                <h1>FriendBook Social Network</h1>
+            <Paper elevation={3} sx={{ my: 4, p: 2 }}>
+                <Typography variant="h1" fontSize="35px" fontWeight="600">FriendBook</Typography>
+                <Typography variant="h2" fontSize="20px" fontWeight="300">Social Network</Typography>
                 <br />
-                <h2>Technologies Used:</h2>
+                <Divider />
+                <br />
+                <Typography variant="h3" fontSize="20px" fontWeight="400">Technologies Used:</Typography>
                 <List>
                     <ListItem disablePadding>
-                        <ListItemButton>
-                            <ListItemText primary="React Framework" />
+                        <ListItemButton component="a" href="https://reactjs.org" target="_blank">
+                            <ListItemIcon>
+                                <DoubleArrowIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="ReactJS" secondary="User interface frontend framework"/>
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
-                        <ListItemButton>
-                            <ListItemText primary="NodeJS (Express)" />
+                        <ListItemButton component="a" href="https://expressjs.com" target="_blank">
+                            <ListItemIcon>
+                                <DoubleArrowIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="NodeJS (Express)" secondary="Backend API server"/>
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
-                        <ListItemButton>
-                            <ListItemText primary="MongoDB" />
+                        <ListItemButton component="a" href="https://www.mongodb.com" target="_blank">
+                            <ListItemIcon>
+                                <DoubleArrowIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="MongoDB" secondary="Relational Database"/>
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
-                        <ListItemButton>
-                            <ListItemText primary="Socket.io" />
+                        <ListItemButton component="a" href="https://socket.io" target="_blank">
+                            <ListItemIcon>
+                                <DoubleArrowIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Socket.io" secondary="2-way communication with backend." />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
-                        <ListItemButton>
-                            <ListItemText primary="Material UI" />
+                        <ListItemButton component="a" href="https://mui.com" target="_blank">
+                            <ListItemIcon>
+                                <DoubleArrowIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Material UI" secondary="UI Design Framework"/>
                         </ListItemButton>
                     </ListItem>
                 </List>
-                <h2>GitHub Links:</h2>
+                <Divider />
+                <br />
+                <Typography variant="h3" fontSize="20px" fontWeight="400">GitHub Links:</Typography>
                 <List>
                     <ListItem disablePadding>
-                        <ListItemButton onClick={() => window.open("https://github.com/ne0rad/friendbook", "_blank")}>
+                        <ListItemButton component="a" href="https://github.com/ne0rad/friendbook" target="_blank">
                             <ListItemIcon>
                                 <GitHubIcon />
                             </ListItemIcon>
-                            <ListItemText primary="Project Source" />
+                            <ListItemText primary="Project Source" secondary="Source code of the project"/>
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
-                        <ListItemButton onClick={() => window.open("https://github.com/ne0rad", "_blank")}>
+                        <ListItemButton component="a" href="https://github.com/ne0rad" target="_blank">
                             <ListItemIcon>
                                 <GitHubIcon />
                             </ListItemIcon>
-                            <ListItemText primary="Profile (ne0rad)" />
+                            <ListItemText primary="Profile (ne0rad)" secondary="My GitHub profile"/>
                         </ListItemButton>
                     </ListItem>
                 </List>
+                <Divider />
+                <br />
+                <Typography>Made by ne0rad</Typography>
             </Paper>
         </Box>
     )
