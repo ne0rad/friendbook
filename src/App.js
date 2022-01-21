38f04cfab622a21e664afcd774f2aa2d42beed90
ne0rad <usackasr@gmail.com>
2022-01-21 10:46:21 +0000

    Added user state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
+import { useState } from "react";
 import { Container } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from '@mui/material/styles';
+import { THEME } from './config/config';
 import Navbar from "./components/Navbar";
+import Main from "./pages/Main";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Signup from "./pages/Signup";
-import { ThemeProvider } from '@mui/material/styles';
-import { THEME } from './config/config';
+import Profile from "./pages/Profile";
 
 
 
 function App() {
 
+  const [user, setUser] = useState();
+
+  function login(username, token) {
+    setUser({ username: username, token: token });
+  }
 
   return (
     <ThemeProvider theme={THEME}>
-      <Navbar />
+      <Navbar user={user} />
       <Container maxWidth="lg" mt="1rem" align="center">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+
+          {user ?
+            // LOGGED IN
+            (<>
+              <Route path="/" element={<Main user={user}/>} />
+              <Route path="/profile" element={<Profile user={user}/>} />
+            </>
+            ) :
+            // NOT LOGGED IN
+            (
+              <>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login login={login} />} />
+                <Route path="/signup" element={<Signup login={login} />} />
+              </>
+            )}
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Container>
