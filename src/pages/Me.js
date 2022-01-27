f7aeda79fe8b45bad762401588c65aac062a0922
ne0rad <usackasr@gmail.com>
2022-01-27 18:47:56 +0000

    Changed spacing

--- src/pages/Me.js
+++ src/pages/Me.js
@@ -7,20 +7,20 @@ function Profile({ user }) {
 
     return (
         <>
-            <Grid container spacing={2} sx={{ my: 1 }} justifyContent="center">
+            <Grid container spacing={1} sx={{ my: 1 }} justifyContent="center">
 
-                <Grid xs={12} md={9} textAlign="left" item>
+                <Grid xs={12} md={8} item>
                     <Details user={user}/>
                 </Grid>
 
-                <Grid xs={12} md={3} item>
+                <Grid xs={12} md={4} item>
                     <Friends />
                 </Grid>
 
             </Grid>
 
             <Grid container spacing={1} sx={{ mt: 1, mb: 3 }}>
-                <Grid xs={12} md={9} item>
+                <Grid xs={12} md={8} item>
                     <Post />
                 </Grid>
             </Grid>
