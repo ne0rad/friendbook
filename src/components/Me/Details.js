55c0cdd98f4e8e5c13ba4acb8be07ef0e126aac8
ne0rad <usackasr@gmail.com>
2022-01-27 18:48:30 +0000

    Changed spacing

--- src/components/Me/Details.js
+++ src/components/Me/Details.js
@@ -1,23 +1,24 @@
 import { Box, Button, Grid, Paper, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
+import EditIcon from '@mui/icons-material/Edit';
+
 
 function Details({ user }) {
     return (
         <Paper elevation={3} sx={{ p: 2 }} >
-            <Grid container spacing={1} justifyContent="space-between">
+            <Grid container spacing={2} justifyContent="space-between">
                 <Grid item xs={12} md={3}>
                     <Box sx={{ bgcolor: grey[200], minWidth: 150, display: "flex", alignItems: "center", justifyContent: "center", borderRadius: "50%" }} width={150} height={150}>
-                        <Typography>No Photo</Typography>
+                        <Typography color="text.secondary">No Photo</Typography>
                     </Box>
                 </Grid>
                 <Grid xs={12} md={9} item>
                     <Typography variant="h4" component="h1" sx={{ wordWrap: "break-word" }}>{user.username}</Typography>
-                    <Typography variant="subtitle1" sx={{ wordWrap: "break-word" }}>Description about yourself etc... asdasd asdas  das ds</Typography>
+                    <Typography variant="subtitle1" color="text.secondary" sx={{ wordWrap: "break-word" }}>Description about yourself etc...</Typography>
                 </Grid>
             </Grid>
-            <Box textAlign="right">
-                <Button variant="outlined">Edit Profile</Button>
-            </Box>
+            <hr />
+            <Button startIcon={<EditIcon />}>Edit Profile</Button>
         </Paper>
     )
 }
