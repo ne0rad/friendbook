6c0ca83854200a89e00aca2f7da6f01e36058d43
ne0rad <usackasr@gmail.com>
2022-01-31 18:17:32 +0000

    Login with token from localStorage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
-import {  Container } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Container } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from '@mui/material/styles';
 import { THEME } from './config/config';
+import { TokenContext } from "./config/context";
+import { API_URI } from "./config/config";
+import axios from "axios";
 import Navbar from "./components/Navbar";
 import Main from "./pages/Main";
 import Home from "./pages/Home";
@@ -10,42 +13,94 @@ import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Signup from "./pages/Signup";
 import Me from "./pages/Me";
-
-
+import Loading from "./pages/Loading";
 
 function App() {
 
-  const [user, setUser] = useState();
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  function login(username, token) {
-    setUser({ username: username, token: token });
+  // Check if user is logged in on page load and set token and user accordingly
+  useEffect(() => {
+    const storageToken = localStorage.getItem('token');
+    if (storageToken) {
+      login(storageToken);
+    } else {
+      setLoading(false);
+    }
+    return () => {
+      setToken(null);
+    }
+  }, []);
+
+  // Login user and set token and user
+  function login(reqToken) {
+    setLoading(true);
+    axios.get(API_URI + "/user/get_user", {
+      headers: {
+        Authorization: `Bearer ${reqToken}`
+      }
+    })
+      .then(res => {
+        if (res.status !== 200) {
+          localStorage.removeItem('token');
+          setToken(null);
+          setUser(null);
+        } else {
+          setToken(reqToken);
+          setUser(res.data);
+          console.log(res);
+          localStorage.setItem('token', reqToken);
+        }
+        setLoading(false);
+      })
+
+      .catch(err => {
+        console.log(err);
+        localStorage.removeItem('token');
+        setLoading(false);
+      });
+  }
+
+  // Logout user and set token and user to null
+  function logout() {
+    setToken(null);
+    setUser(null);
+    localStorage.removeItem('token');
   }
 
   return (
     <ThemeProvider theme={THEME}>
-      <Navbar user={user} />
-      <Container maxWidth="lg" mt="1rem" p="0" align="center">
-        <Routes>
+      <TokenContext.Provider value={token}>
+        {loading ? <Loading /> : (
+          <>
+            <Navbar logout={logout} />
+            <Container maxWidth="lg" mt="1rem" p="0" align="center">
+              <Routes>
 
-          {user ?
-            // LOGGED IN
-            (<>
-              <Route path="/" element={<Main user={user}/>} />
-              <Route path="/me" element={<Me user={user}/>} />
-            </>
-            ) :
-            // NOT LOGGED IN
-            (
-              <>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login login={login} />} />
-                <Route path="/signup" element={<Signup login={login} />} />
-              </>
-            )}
+                {token ?
+                  // LOGGED IN
+                  (<>
+                    <Route path="/" element={<Main user={user} />} />
+                    <Route path="/me" element={<Me />} />
+                  </>
+                  ) :
+                  // NOT LOGGED IN
+                  (
+                    <>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/login" element={<Login login={login} />} />
+                      <Route path="/signup" element={<Signup login={login} />} />
+                    </>
+                  )}
 
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Container>
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Container>
+          </>
+        )}
+      </TokenContext.Provider>
     </ThemeProvider>
   );
 }
