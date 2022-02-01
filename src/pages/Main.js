2c6ee97f94999d9c7fe816bbdcd328286839c24a
ne0rad <usackasr@gmail.com>
2022-02-01 17:29:15 +0000

    Testing user state

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,18 @@
 import { Paper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import Post from "../components/Post";
+// import { useContext } from "react";
+// import { TokenContext } from "../config/context";
 
 function Main({ user }) {
+
+    //const token = useContext(TokenContext);
+
     return (
         <Box maxWidth="sm">
             <Paper elevation={3} sx={{ my: 4, p: 2 }}>
                 <Typography variant="h3">Main Page</Typography>
-                <Typography>Hello, {user.username}</Typography>
+                <Typography>Username: {user.username}</Typography>
             </Paper>
             <Post />
         </Box>
