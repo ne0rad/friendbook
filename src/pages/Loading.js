dc1ed8bb7cc95e9e96946e4bc5ded93e72baba0e
ne0rad <usackasr@gmail.com>
2022-02-01 17:28:13 +0000

    Updated Loading component

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,11 +1,28 @@
-import { CircularProgress, Container } from "@mui/material";
+import { CircularProgress, Container, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
 
 function Loading() {
+
+    const [tooLong, setTooLong] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTooLong(true);
+        }, 4000);
+        return () => {
+            setTooLong(false);
+            clearTimeout(timer);
+        }
+    }, []);
+
     return (
         <Container maxWidth="lg" mt="1rem" p="0" align="center">
-            <Box maxWidth="sm" sx={{p: 4}}>
-                    <CircularProgress />
+            <Box maxWidth="sm" sx={{ p: 4 }}>
+                <CircularProgress />
+                <Typography mt="1rem">
+                    {!tooLong ? "Loading..." : "Still loading... Hold on, backend is probably asleep."}
+                </Typography>
             </Box >
         </Container>
     )
