0ba7c602925b4057c759566cee32429231022cd5
ne0rad <usackasr@gmail.com>
2022-02-05 10:53:02 +0000

    Added notification and messages icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,27 @@
-import { AppBar, Box, Toolbar, Typography, Button, IconButton, Tooltip, Zoom } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, Button, IconButton, Tooltip, Zoom, Badge } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import HowToRegIcon from '@mui/icons-material/HowToReg';
 import LoginIcon from '@mui/icons-material/Login';
 import NavMenu from './NavMenu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MessageIcon from '@mui/icons-material/Message';
 import { useContext } from 'react';
 import { TokenContext } from '../config/context';
 
 function NavBar({ loading }) {
     const navigate = useNavigate();
     const token = useContext(TokenContext);
+
+    const StyledBadge = styled(Badge)(() => ({
+        '& .MuiBadge-badge': {
+            right: 2,
+            top: 2,
+            border: `2px solid white`,
+            padding: '0 4px',
+        },
+    }));
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -37,6 +49,30 @@ function NavBar({ loading }) {
                                             <HomeIcon />
                                         </IconButton>
                                     </Tooltip>
+                                    <Tooltip title="Messages" TransitionComponent={Zoom} arrow>
+                                        <IconButton
+                                            aria-label="Messages"
+                                            size="large"
+                                            sx={{ color: 'white' }}
+                                        >
+                                            <StyledBadge badgeContent={11} color="primary">
+                                                <MessageIcon />
+                                            </StyledBadge>
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Tooltip title="Notifications" TransitionComponent={Zoom} arrow>
+
+                                        <IconButton
+                                            aria-label="Notifications"
+                                            size="large"
+                                            sx={{ color: 'white' }}
+                                        >
+                                            <StyledBadge badgeContent={2} color="primary">
+                                                <NotificationsIcon />
+                                            </StyledBadge>
+                                        </IconButton>
+                                    </Tooltip>
+
                                     <NavMenu />
                                 </>
                             ) :
