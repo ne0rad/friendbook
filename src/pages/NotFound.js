f88b02ca521b076304e5af4799559bac56f2d774
ne0rad <usackasr@gmail.com>
2022-02-07 13:16:35 +0000

    Updated margins

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,7 @@ import { Box, Paper, Typography } from '@mui/material';
 function NotFound() {
     return (
         <Box maxWidth="sm">
-            <Paper elevation={3} sx={{ my: 4, p: 4 }}>
+            <Paper elevation={3} sx={{ p: 4 }}>
                 <Typography variant='h1'>404</Typography>
                 <Typography variant='h4'>Page Not Found</Typography>
             </Paper>
