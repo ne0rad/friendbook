cb73880082d14a08a13f5dd1beb61b084790ea8e
ne0rad <usackasr@gmail.com>
2022-03-12 21:34:46 +0000

    MessageDrawer uses socket connection

--- src/components/MessagesDrawer.js
+++ src/components/MessagesDrawer.js
@@ -1,39 +1,10 @@
-import { Box, Button, ButtonGroup, Drawer, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
-import MarkEmailUnreadIcon from '@mui/icons-material/MarkEmailUnread';
-import DraftsOutlinedIcon from '@mui/icons-material/DraftsOutlined';
+import { Box, Button, Drawer } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import ChatList from "./Chat/ChatList";
 
 function MessagesDrawer({ openMessages, toggleMessages }) {
 
     const navigate = useNavigate();
-
-    const list = () => (
-        <Box
-            sx={{ width: 300 }}
-            role="presentation"
-            onClick={() => toggleMessages()}
-            onKeyDown={() => toggleMessages()}
-        >
-            <List>
-
-                <ListItem sx={{ mb: 1 }}>
-                    <ButtonGroup variant="outlined" aria-label="message functions" sx={{ alignItems: 'center' }}>
-                        <Button>New Message</Button>
-                        <Button onClick={() => navigate('/messages')}>All Messages</Button>
-                    </ButtonGroup>
-                </ListItem>
-
-                {['qwe', 'admin'].map((text, index) => (
-                    <ListItem button key={text} divider sx={{ px: 1, py: 0 }}>
-                        <ListItemIcon >
-                            {index === 1 ? <DraftsOutlinedIcon /> : <MarkEmailUnreadIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} secondary="Hey, I just wanted to let you kno..." />
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
 
     return (
         <>
@@ -42,7 +13,22 @@ function MessagesDrawer({ openMessages, toggleMessages }) {
                 open={openMessages}
                 onClose={() => toggleMessages()}
             >
-                {list('right')}
+                <Box
+                    sx={{ width: 300 }}
+                    role="presentation"
+                    onClick={() => toggleMessages()}>
+
+                    <Button
+                        sx={{ width: '100%', p: 1 }}
+                        onClick={() => {
+                            navigate('/messages')
+                        }}>
+                        All chats
+                    </Button>
+
+                    <ChatList />
+
+                </Box>
             </Drawer>
         </>
     )
