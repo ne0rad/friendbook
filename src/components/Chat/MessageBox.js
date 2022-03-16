98554befa0c3a3aeac8344915799473de90b508d
ne0rad <usackasr@gmail.com>
2022-03-16 22:28:50 +0000

    Moved some code to Message component

--- src/components/Chat/MessageBox.js
+++ src/components/Chat/MessageBox.js
@@ -1,63 +1,34 @@
-import { Box, Paper, Typography } from "@mui/material";
-import { useContext, useEffect, useRef } from "react";
-import { blue, green } from '@mui/material/colors';
-import { UserContext } from "../../config/context";
+import { Paper } from "@mui/material";
+import Message from "./Message";
+import { useEffect, useRef } from "react";
+
 
 function MessageBox({ messages }) {
 
-    const messagesEndRef = useRef(null)
-    const user = useContext(UserContext);
-
-    const scrollToBottom = () => {
-        messagesEndRef.current?.scrollIntoView();
-    }
+    const messagesBottom = useRef(null);
 
     useEffect(() => {
-        scrollToBottom();
+        messagesBottom.current?.scrollIntoView();
     }, [messages]);
 
     return (
         <Paper
             elevation={0}
+            id="scrollablePaper"
             sx={{
                 backgroundColor: "#f2f2f2",
-                height: "70vh",
                 my: 2,
-                p: 1,
+                px: 0,
+                py: 1,
                 border: "1px solid #e0e0e0",
-                overflowY: "scroll",
-                "&::-webkit-scrollbar": {
-                    display: "none"
-                }
+                height: '70vh',
+                overflow: "auto"
             }}>
-
             {
-                messages.map((message, index) => {
-                    const isMe = message.author === user.username;
-                    const author = isMe ? "You" : message.author;
-                    return (
-                        <Box
-                            key={index}
-                            sx={{ m: 1, display: 'flex', justifyContent: isMe ? 'flex-start' : 'flex-end' }}
-                        >
-                            <Typography
-                                sx={{
-                                    p: 1,
-                                    borderRadius: "5px",
-                                    width: "fit-content",
-                                    maxWidth: "80%",
-                                    wordBreak: "break-all",
-                                    backgroundColor: isMe ? blue[100] : green[100]
-                                }}
-                            >
-                                <b>{author}:</b> {message.message}
-                            </Typography>
-                        </Box>
-                    )
-                })
+                messages.map((message, index) => <Message message={message} key={index} />)
             }
-            <div ref={messagesEndRef} />
-        </Paper>
+            <div ref={messagesBottom} />
+        </Paper >
     )
 }
 
