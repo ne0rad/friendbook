c16ea890f7b3586545b6db0ad76b50dff483e11b
ne0rad <usackasr@gmail.com>
2022-04-03 21:58:36 +0100

    Updated Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import { LoadingPage, NotFoundPage } from "./pages";
 import React from "react";
 
-// Lazy load pages
-const LandingRouter: React.ExoticComponent = React.lazy(
-  () => import("./pages/Landing/LandingRouter")
-);
-const MainRouter: React.ExoticComponent = React.lazy(
-  () => import("./pages/Main/MainRouter")
-);
+const LandingRouter = React.lazy(() => import("./pages/Landing/LandingRouter"));
+const MainRouter = React.lazy(() => import("./pages/Main/MainRouter"));
 
 type Props = {
   loggedIn: boolean;
@@ -16,32 +11,16 @@ type Props = {
 
 export default function Router({ loggedIn }: Props): JSX.Element {
   return (
-    <Routes>
-      {loggedIn ? (
-        <>
-          <Route
-            path="/"
-            element={
-              <React.Suspense fallback={<LoadingPage />}>
-                <MainRouter />
-              </React.Suspense>
-            }
-          />
-        </>
-      ) : (
-        <>
-          <Route
-            path="/*"
-            element={
-              <React.Suspense fallback={<LoadingPage />}>
-                <LandingRouter />
-              </React.Suspense>
-            }
-          />
-        </>
-      )}
+    <React.Suspense fallback={<LoadingPage />}>
+      <Routes>
+        {loggedIn ? (
+          <Route path="/" element={<MainRouter />} />
+        ) : (
+          <Route path="/*" element={<LandingRouter />} />
+        )}
 
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </React.Suspense>
   );
 }
