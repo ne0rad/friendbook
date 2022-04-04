c38af6ad87002fc23822991046048ecf10531210
ne0rad <usackasr@gmail.com>
2022-04-04 17:44:08 +0100

    .

--- src/pages/landing/components/index.tsx
+++
@@ -1,8 +0,0 @@
-import AuthButtons from "./AuthButtons";
-import FormBox from "./FormBox";
-import LoginForm from "./LoginForm";
-import MainBox from "./MainBox";
-import SignupForm from "./SignupForm";
-import TitleText from "./TitleText";
-
-export { AuthButtons, FormBox, LoginForm, MainBox, SignupForm, TitleText };
