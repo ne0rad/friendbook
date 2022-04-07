103e0a2adefc6ee4987df3f3764ec581e70631d8
ne0rad <usackasr@gmail.com>
2022-04-07 16:33:00 +0100

    Added Auth component

---
+++ src/components/Auth.tsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { API_URI } from "../config";
+import { AuthContext } from "../context";
+
+type Props = {
+  children: JSX.Element;
+};
+
+export default function Auth({ children }: Props) {
+  const navigate = useNavigate();
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const tokenLogin = useCallback(async (token: string) => {
+    axios
+      .post(`/auth/token_login`, { token })
+      .then((res) => {
+        if (res.status === 200) {
+          setLoggedIn(true);
+          setLoading(false);
+          localStorage.setItem("token", res.data.token);
+          axios.defaults.headers.common["Authorization"] = token;
+        }
+      })
+      .catch((err) => {
+        setLoggedIn(false);
+        setLoading(false);
+        localStorage.removeItem("token");
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.defaults.baseURL = API_URI;
+  }, []);
+
+  useEffect(() => {
+    if (loading) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        tokenLogin(token);
+      } else {
+        setLoading(false);
+      }
+    }
+  }, [tokenLogin, loading]);
+
+  function authContextLogout(): void {
+    localStorage.removeItem("token");
+    setLoggedIn(false);
+  }
+
+  function authContextLogin(token: string): void {
+    localStorage.setItem("token", token);
+    navigate("/");
+    setLoading(true);
+  }
+
+  return (
+    <>
+      <AuthContext.Provider
+        value={{
+          loggedIn,
+          loading,
+          login: authContextLogin,
+          logout: authContextLogout,
+        }}
+      >
+        {children}
+      </AuthContext.Provider>
+    </>
+  );
+}
---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+import Auth from "./Auth";
+import Router from "./Router";
+
+export { Auth, Router };
