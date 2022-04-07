93543a90b77244565db037521572df243d243ffb
ne0rad <usackasr@gmail.com>
2022-04-07 16:41:41 +0100

    Updated index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import { createRoot } from "react-dom/client";
-
 // Hash router is required to deploy on gh-pages
 import { HashRouter } from "react-router-dom";
+import { Auth, Router } from "./components";
 
 import "./index.css";
-import App from "./App";
 
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
 
 root.render(
   <HashRouter>
-    <App />
+    <Auth>
+      <Router />
+    </Auth>
   </HashRouter>
 );
